e5d89f1df04b345a064af811bc9a665df5b93ed6
vinicius silva <vinicius_dev@hotmail.com>
2022-08-05 17:41:15 -0300

    feat: Utilizada estado global no app ( troca de pagina )

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import './App.css';
+import { useContext } from 'react';
+import { CustomerContext } from './globalState/CostumerProvider'
 import './styles/Page.css'
+
 import NavBackground from './components/NavBackground';
+
+import MeusProjetos from './pages/MeusProjetos';
+import OQueEuNaoSei from './pages/OQueEuNaoSei';
+import OQueEuQuero from './pages/OQueEuQuero';
 import QuemEuSou from './pages/QuemEuSou';
+import OQueEuSei from './pages/OQueEuSeiFazer';
+import Intro from './pages/Intro';
 
 function App() {
+  const { eu, quero, projetos, naoSei, sei, intro } = useContext(CustomerContext)
+
   return (
     <div>
       <div>
         <NavBackground />
       </div>
       <div className='page'>   
-        <QuemEuSou />
+        { intro && <Intro />}
+        { projetos && <MeusProjetos /> }
+        { naoSei && <OQueEuNaoSei /> }
+        { sei  && <OQueEuSei /> }
+        { eu && <QuemEuSou />}
+        { quero && <OQueEuQuero />}
       </div>
     </div>
   );
