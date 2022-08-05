0a3f8af2bc4418d88bcf7d0eb685e30697450043
vinicius silva <vinicius_dev@hotmail.com>
2022-08-05 17:39:30 -0300

    feat: Implementa estados necessarios para mavegacao

--- src/globalState/CostumerProvider.js
+++ src/globalState/CostumerProvider.js
@@ -4,9 +4,19 @@ export const CustomerContext = createContext();
 
 export default function CostumerProvider({ children }) {
   const [ eu, setEu ] = useState(false)
+  const [ quero, setQuero ] = useState(false)
+  const [ projetos, setProjetos ] = useState(false)
+  const [ naoSei, setNaoSei ] = useState(false)
+  const [ sei, setSei ] = useState(false)
+  const [intro, setIntro ] = useState( true )
 
   const context = {
     eu, setEu,
+    quero, setQuero,
+    projetos, setProjetos,
+    naoSei, setNaoSei,
+    sei, setSei,
+    intro, setIntro,
   }
 
   return(
