d49f6bd0b6147ee206049f09c4278a4e0c649a48
vinicius silva <vinicius_dev@hotmail.com>
2022-08-05 17:46:13 -0300

    feat: Implementar funcao para troca de pagina

--- src/components/NavBackground.tsx
+++ src/components/NavBackground.tsx
@@ -1,15 +1,27 @@
 import '../styles/NavBackground.css'
 import ItemOption from './ItemOption'
+import { useContext } from 'react'
+import { CustomerContext } from '../globalState/CostumerProvider'
+import navigateTo from '../utils/navFunctions'
 
 export default function NavBackground(){
+  
+  const { setEu, setQuero, setProjetos, setNaoSei, setSei, setIntro } = useContext(CustomerContext)
+
+  const eu = () => navigateTo(setEu, [ setQuero, setProjetos, setNaoSei, setSei, setIntro ])
+  const quero = () => navigateTo(setQuero, [ setEu, setProjetos, setNaoSei, setSei, setIntro ])
+  const projetos = () => navigateTo(setProjetos, [ setEu, setQuero, setNaoSei, setSei, setIntro ])
+  const naoSei = () => navigateTo(setNaoSei, [ setEu, setQuero, setProjetos, setSei, setIntro ])
+  const sei = () => navigateTo(setSei, [ setEu, setQuero, setProjetos, setNaoSei, setIntro ])
+
   return(
     <div className='navBackground blur' >
       <nav className='container'>
-        <ItemOption text='Quem eu Sou eu'/>
-        <ItemOption text='O que eu quero'/>
-        <ItemOption text='O que eu sei fazer'/>
-        <ItemOption text='O que eu não sei fazer'/>
-        <ItemOption text='Meus projetos'/>
+        <ItemOption text='Quem eu Sou eu' click={ eu }/>
+        <ItemOption text='O que eu quero' click={ quero }/>
+        <ItemOption text='O que eu sei fazer' click={ sei } />
+        <ItemOption text='O que eu não sei fazer' click={ naoSei }/>
+        <ItemOption text='Meus projetos' click={ projetos }/>
         <ItemOption text='Porque do portifolio ser assim'/>
         <ItemOption text='Feebacks'/>
       </nav>
