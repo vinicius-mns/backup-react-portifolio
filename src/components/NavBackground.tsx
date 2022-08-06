d97f50290d6d396ccf7c4a6b6fed7c7d0665dac9
vinicius silva <vinicius_dev@hotmail.com>
2022-08-06 17:41:10 -0300

    feat: Implementa classe dinamicamente ao `itemOption`

--- src/components/NavBackground.tsx
+++ src/components/NavBackground.tsx
@@ -8,6 +8,9 @@ export default function NavBackground(){
   
   const { setEu, setQuero, setProjetos, setNaoSei, setSei, setIntro } = useContext(CustomerContext)
 
+  const { eu: e, quero: q, projetos: p, naoSei: n, sei: s } = useContext(CustomerContext)
+
+
   const eu = () => navigateTo(setEu, [ setQuero, setProjetos, setNaoSei, setSei, setIntro ])
   const quero = () => navigateTo(setQuero, [ setEu, setProjetos, setNaoSei, setSei, setIntro ])
   const projetos = () => navigateTo(setProjetos, [ setEu, setQuero, setNaoSei, setSei, setIntro ])
@@ -17,11 +20,11 @@ export default function NavBackground(){
   return(
     <div className='navBackground' >
       <nav className='container'>
-        <ItemOption text='Quem eu Sou eu' click={ eu }/>
-        <ItemOption text='O que eu quero' click={ quero }/>
-        <ItemOption text='O que eu sei fazer' click={ sei } />
-        <ItemOption text='O que eu não sei fazer' click={ naoSei }/>
-        <ItemOption text='Meus projetos' click={ projetos }/>
+        <ItemOption text='Quem eu Sou eu' click={ eu } active={e}/>
+        <ItemOption text='O que eu quero' click={ quero } active={q}/>
+        <ItemOption text='O que eu sei fazer' click={ sei } active={s}/>
+        <ItemOption text='O que eu não sei fazer' click={ naoSei } active={n}/>
+        <ItemOption text='Meus projetos' click={ projetos } active={p}/>
         <ItemOption text='Porque do portifolio ser assim'/>
         <ItemOption text='Feebacks'/>
       </nav>
