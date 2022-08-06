da59ea0b98698b471c0704f760a25a7829d3f795
vinicius silva <vinicius_dev@hotmail.com>
2022-08-06 17:40:02 -0300

    feat: Adiciona classe dinamicamente ao component `itemOption`

--- src/components/ItemOption.tsx
+++ src/components/ItemOption.tsx
@@ -1,6 +1,6 @@
-export default function ItemOption({text, click, m}: any){
+export default function ItemOption({text, click, active}: any){
   return (
-    <button className='itemOption' onClick={click}>
+    <button className={`itemOption ${active}`} onClick={click}>
       <h3>{text}</h3>
     </button>
   )
