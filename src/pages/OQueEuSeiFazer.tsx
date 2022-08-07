01d7838ca3145189c1a02ab6eeaed537dfc9412e
vinicius silva <vinicius_dev@hotmail.com>
2022-08-07 02:40:57 -0300

    feat: Mudando nomes de classe para melhor legibilidade

--- src/pages/OQueEuSeiFazer.tsx
+++ src/pages/OQueEuSeiFazer.tsx
@@ -3,12 +3,12 @@ import image from '../midia/apontando.gif'
 
 export default function OQueEuSei() {
   return (
-    <div className="OQueEuSei">
-      <div className="lado a">
-        <div className='xx'>
+    <div className="container_oqs">
+      <div className="side a">
+        <div className='card_img'>
           <img src={image} alt="eu" />
         </div>
-        <div className='yy'>
+        <div className='sub_container_oqs'>
           <h1>Competencias</h1>
           <p>Eu sou incompetente em tudossssssssssssssssssssssssssssssssssssss</p>
           <br />
@@ -37,8 +37,8 @@ export default function OQueEuSei() {
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum quo explicabo ab corporis maxime? Ut a provident blanditiis perferendis temporibus dicta! Eos suscipit aliquid voluptatum illo recusandae totam expedita dolorem?</p>
         </div>
       </div>
-      <div className="lado">
-        <a className='certificado' 
+      <div className="side">
+        <a className='certificate' 
           href='https://www.credential.net/57368f52-2ce0-4003-b4f4-bae0209a995e#gs.7xplpt'
           target='blank'
         >
@@ -48,7 +48,7 @@ export default function OQueEuSei() {
             alt="Certificado"
           />
         </a>
-        <a className='certificado'
+        <a className='certificate'
           href='https://www.credential.net/c2a6609d-1c9e-4ff7-88a4-669dfa23fe2d#gs.7xps3x'
           target='blank'
         >
@@ -56,10 +56,9 @@ export default function OQueEuSei() {
           <img 
             src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/42886866"
             alt="Certificado"
-            className='certificado'
           />
         </a>
-        <a className='certificado'
+        <a className='certificate'
           href='https://www.credential.net/0ee03b0c-bf3c-432a-b17c-fbbdcce92198#gs.7xps4h'
           target='blank'
         >
@@ -67,7 +66,6 @@ export default function OQueEuSei() {
           <img 
             src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/54650665"
             alt="Certificado"
-            className='certificado'
           />
         </a>
       </div>
