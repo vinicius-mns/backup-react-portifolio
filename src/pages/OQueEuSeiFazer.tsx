00f95e3e3ad533168a2b3b16e350a5762332d529
vinicius silva <vinicius_dev@hotmail.com>
2022-08-07 02:58:50 -0300

    feat: Implementa informacoes na pagina `OQue eu sei`

--- src/pages/OQueEuSeiFazer.tsx
+++ src/pages/OQueEuSeiFazer.tsx
@@ -38,36 +38,18 @@ export default function OQueEuSei() {
         </div>
       </div>
       <div className="side">
-        <a className='certificate' 
-          href='https://www.credential.net/57368f52-2ce0-4003-b4f4-bae0209a995e#gs.7xplpt'
-          target='blank'
-        >
-          <h2>Fundamentos</h2>
-          <img 
-            src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/38013201"
-            alt="Certificado"
-          />
-        </a>
-        <a className='certificate'
-          href='https://www.credential.net/c2a6609d-1c9e-4ff7-88a4-669dfa23fe2d#gs.7xps3x'
-          target='blank'
-        >
-          <h2>Front-End</h2>
-          <img 
-            src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/42886866"
-            alt="Certificado"
-          />
-        </a>
-        <a className='certificate'
-          href='https://www.credential.net/0ee03b0c-bf3c-432a-b17c-fbbdcce92198#gs.7xps4h'
-          target='blank'
-        >
-          <h2>Backend-End</h2>
-          <img 
-            src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/54650665"
-            alt="Certificado"
-          />
-        </a>
+        { certificados.map( ( { titulo, link, img } ) => (
+          <a className='certificate' 
+            href={link}
+            target='blank'
+            >
+            <h2>{titulo}</h2>
+            <img 
+              src={img}
+              alt={`Certificado de ${titulo}`}
+            />
+          </a>
+        ) ) }
       </div>
     </div>
   )
