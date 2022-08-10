fab9c63149afa6ee3df92fa52fc834ccd52e4d74
vinicius silva <vinicius_dev@hotmail.com>
2022-08-10 16:18:14 -0300

    refactor: mudando conteudo para propria pagina

---
+++ src/pages/QuemEuSou/content.ts
@@ -0,0 +1,12 @@
+import image from '../../midia/avatarg.gif'
+
+const img_description = 'um modelo humanoide 3d respresentando o autor do portifolio'
+
+const i_am = `Me chamo Vinicius(ele / dele) - Programador júnior, atualmente estudando Python na Trybe.
+Curioso desde criança, quebrava todos os meus brinquedos para ver como funcionavam, hoje não quebro mais nada, porém ainda curioso. Programação era o meu destino e eu não sabia. Eu queria ser inventor, e inventar coisa nova é o que eu mais faço`
+
+export {
+  img_description,
+  i_am,
+  image,
+}
--- src/pages/QuemEuSou/index.tsx
+++ src/pages/QuemEuSou/index.tsx
@@ -1,18 +1,15 @@
-import image from '../../midia/avatarg.gif'
 import './style.css'
+import { img_description, i_am, image } from './content'
 
 export default function QuemSouEu() {
-  const desc = 'um modelo humanoide 3d respresentando o autor do portifolio'
-  const sobre = `Me chamo Vinicius(ele / dele) - Programador júnior, atualmente estudando Python na Trybe.
-  Curioso desde criança, quebrava todos os meus brinquedos para ver como funcionavam, hoje não quebro mais nada, porém ainda curioso. Programação era o meu destino e eu não sabia. Eu queria ser inventor, e inventar coisa nova é o que eu mais faço`
   return(
     <div className='box displayFlex'>
       <div className='quadro'>
-          <img className='avatar' src={image} alt={desc} />
+          <img className='avatar' src={image} alt={img_description} />
       </div>
         <h1>V i n í c i u s</h1>
       <div className='text'>
-          <p>{sobre}</p>
+          <p>{i_am}</p>
     </div>
   </div>
   )
