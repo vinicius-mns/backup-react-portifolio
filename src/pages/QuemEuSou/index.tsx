4e14c5628f70e62f0e4bda7f324ae0e4913b05ab
vinicius silva <vinicius_dev@hotmail.com>
2022-08-12 00:30:20 -0300

    feat: Adicionando meu pitch

--- src/pages/QuemEuSou/index.tsx
+++ src/pages/QuemEuSou/index.tsx
@@ -3,13 +3,13 @@ import { img_description, i_am, image } from './content'
 
 export default function QuemSouEu() {
   return(
-    <div className='box displayFlex'>
+    <div className='box'>
       <div className='quadro'>
           <img className='avatar' src={image} alt={img_description} />
       </div>
         <h1>V i n í c i u s</h1>
       <div className='text'>
-          <p>{i_am}</p>
+          { i_am.map((me) => <p>{ me }</p>) }
     </div>
   </div>
   )
