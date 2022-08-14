a8c0dda0859fd9fd3452ae4c4ca715dc67a9e746
vinicius silva <vinicius_dev@hotmail.com>
2022-08-14 20:46:02 -0300

    fix: Coloca o Titulo dentro do text

--- src/pages/QuemEuSou/index.tsx
+++ src/pages/QuemEuSou/index.tsx
@@ -7,9 +7,9 @@ export default function QuemSouEu() {
       <div className='quadro'>
           <img className='avatar' src={image} alt={img_description} />
       </div>
-        <h1>V i n í c i u s</h1>
       <div className='text'>
-          { i_am.map((me) => <p>{ me }</p>) }
+        <h1>V i n í c i u s</h1>
+        { i_am.map((me) => <p>{ me }</p>) }
     </div>
   </div>
   )
