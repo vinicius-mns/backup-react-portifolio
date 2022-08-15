c0f64b957d3d2368cf9cfbaaa5aa89d328b41c94
vinicius silva <vinicius_dev@hotmail.com>
2022-08-15 10:53:08 -0300

    feat: Implementa midiaHub em Routes

--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -5,12 +5,15 @@ import Intro from "../pages/Intro"
 import QuemSouEu from "../pages/QuemEuSou"
 import OQueEuSei from "../pages/OQueEuSei"
 
+import imgs from '../midia/midiaHub'
+
 export default function MyRoutes() {
+
   return(
     <Routes>
       <Route path='/' element={ <Intro /> } />
-      <Route path={ myPath.quemSou } element={ <QuemSouEu /> } />
-      <Route path={ myPath.oQueSei } element={ <OQueEuSei /> } />
+      <Route path={ myPath.quemSou } element={ <QuemSouEu avatar={imgs.eu} /> } />
+      <Route path={ myPath.oQueSei } element={ <OQueEuSei avatar={imgs.teclando} /> } />
     </Routes> 
   )
 }
