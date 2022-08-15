d1f41a4050ea667f39f61be6d448611da08df0f4
vinicius silva <vinicius_dev@hotmail.com>
2022-08-15 10:18:00 -0300

    feat: Implementa Rotas ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import './App.css';
-import { useContext } from 'react';
-import { CustomerContext } from './globalState/CostumerProvider'
 import './styles/Page.css'
 
 import NavBackground from './components/NavBackground';
 
-import MeusProjetos from './pages/MeusProjetos';
-import OQueEuNaoSei from './pages/OQueEuNaoSei';
-import OQueEuQuero from './pages/OQueEuQuero';
-import QuemEuSou from './pages/QuemEuSou';
-import OQueEuSei from './pages/OQueEuSei';
-import Intro from './pages/Intro';
+import MyRoutes from './routes/MyRoutes';
 
 function App() {
-  const { eu, quero, projetos, naoSei, sei, intro } = useContext(CustomerContext)
-
   return (
     <div>
       <div>
         <NavBackground />
       </div>
-      <div className='page'>   
-        { intro && <Intro />}
-        { projetos && <MeusProjetos /> }
-        { naoSei && <OQueEuNaoSei /> }
-        { sei  && <OQueEuSei /> }
-        { eu && <QuemEuSou />}
-        { quero && <OQueEuQuero />}
+      <div className='page'>
+        <MyRoutes />
       </div>
     </div>
   );
