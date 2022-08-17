f3f997fc6cd4709fdc37063c115ec99d2e36e2a7
vinicius silva <vinicius_dev@hotmail.com>
2022-08-17 03:57:18 -0300

    feat: Adiciona email aos contatos

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './style.css'
 
 // interface Props extends React.Component {
@@ -5,14 +6,23 @@ import './style.css'
 // }
 
 export default function Contacts({resolve}: any ) {
+  const [ e, setE ] = useState(false)
+
   return (
-    <div className={`contacts ${resolve}`}>
+    <div className={resolve}>
       <a href="https://www.linkedin.com/in/vinicius-dev-junior/" target='blank'>
         <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkedin" />
       </a>
       <a href="https://github.com/vinicius-mns" target='blank'>
         <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github" />
       </a>
+      <img 
+        src="https://cdn-icons-png.flaticon.com/512/561/561127.png"
+        alt="Email"
+        onMouseEnter={() => setE( true )}
+        onMouseLeave={() => setE( false )}
+      />
+      { e && <h2 className='email' >vinicius_dev@hotmail.com</h2> }
     </div>
   )
 }
