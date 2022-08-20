9c3c3bb3980e76b8ac1c7381327a7dfd6e779e2c
vinicius silva <vinicius_dev@hotmail.com>
2022-08-20 00:02:53 -0300

    refactor: lint: paginas em branco

--- src/pages/MeusProjetos/index.tsx
+++ src/pages/MeusProjetos/index.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
+
 export default function MeusProjetos() {
-  return (
-    <div>
-      <h1>Meus Projetos</h1>
-    </div>
-  )
+	return (
+		<div>
+			<h1>Meus Projetos</h1>
+		</div>
+	)
 }
