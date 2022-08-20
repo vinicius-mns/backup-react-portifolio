ad8780908a2fae5cc49ab34a9aef2525684afb56
vinicius silva <vinicius_dev@hotmail.com>
2022-08-20 00:15:41 -0300

    refactor: lint: global state

--- src/globalState/CostumerProvider.js
+++ src/globalState/CostumerProvider.js
@@ -1,27 +1,29 @@
-import { useState, createContext } from "react"
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/prop-types */
+import { useState, createContext } from 'react'
 
-export const CustomerContext = createContext();
+export const CustomerContext = createContext()
 
 export default function CostumerProvider({ children }) {
-  const [ eu, setEu ] = useState(false)
-  const [ quero, setQuero ] = useState(false)
-  const [ projetos, setProjetos ] = useState(false)
-  const [ naoSei, setNaoSei ] = useState(false)
-  const [ sei, setSei ] = useState(false)
-  const [intro, setIntro ] = useState( true )
+	const [ eu, setEu ] = useState( false )
+	const [ quero, setQuero ] = useState( false )
+	const [ projetos, setProjetos ] = useState( false )
+	const [ naoSei, setNaoSei ] = useState( false )
+	const [ sei, setSei ] = useState( false )
+	const [ intro, setIntro ] = useState( true )
 
-  const context = {
-    eu, setEu,
-    quero, setQuero,
-    projetos, setProjetos,
-    naoSei, setNaoSei,
-    sei, setSei,
-    intro, setIntro,
-  }
+	const context = {
+		eu, setEu,
+		quero, setQuero,
+		projetos, setProjetos,
+		naoSei, setNaoSei,
+		sei, setSei,
+		intro, setIntro,
+	}
 
-  return(
-    <CustomerContext.Provider value={ context }>
-      { children }
-    </CustomerContext.Provider>
-  )
+	return(
+		<CustomerContext.Provider value={ context }>
+			{ children }
+		</CustomerContext.Provider>
+	)
 }
