51d3d5c041cd4ecf9c80ad212084291339ce3c51
vinicius silva <vinicius_dev@hotmail.com>
2022-08-19 23:42:11 -0300

    refactor: lint: Barra de navegacao

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,22 +1,23 @@
+import React from 'react'
 import './style.css'
 import ItemOption from '../ItemOption'
 import { useNavigate } from 'react-router-dom'
 import myPath from '../../routes/myPaths'
 
 export default function NavBackground(){
-  const navigate = useNavigate()
+	const navigate = useNavigate()
 
-  return(
-    <div className='navBackground' >
-      <nav className='container button-group'>
-        <ItemOption text='Quem Sou eu' click={ () => navigate(myPath.quemSou) } />
-        <ItemOption text='O que eu sei fazer' click={ () => navigate(myPath.oQueSei) } />
-        <ItemOption text='O que eu quero' click={ () => navigate(myPath.oQQuero) } />
-        <ItemOption text='O que eu não sei fazer' click={ () => navigate(myPath.oQueNaoSei) } />
-        <ItemOption text='Meus projetos' click={ () => navigate(myPath.projetos) } />
-        <ItemOption text='Porque do portifolio ser assim'/>
-        <ItemOption text='Feebacks'/>
-      </nav>
-    </div>
-  )
+	return(
+		<div className='navBackground' >
+			<nav className='container button-group'>
+				<ItemOption text='Quem Sou eu' click={ () => navigate( myPath.quemSou ) }/>
+				<ItemOption text='O que eu sei fazer' click={ () => navigate( myPath.oQueSei ) }/>
+				<ItemOption text='O que eu quero' click={ () => navigate( myPath.oQQuero ) }/>
+				<ItemOption text='O que eu não sei fazer' click={ () => navigate( myPath.oQueNaoSei ) }/>
+				<ItemOption text='Meus projetos' click={ () => navigate( myPath.projetos ) }/>
+				<ItemOption text='Porque do portifolio ser assim'/>
+				<ItemOption text='Feebacks'/>
+			</nav>
+		</div>
+	)
 }
