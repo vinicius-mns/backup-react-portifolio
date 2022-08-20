694d3a638a4e701d71050fcb4cf41701a33dbfe0
vinicius silva <vinicius_dev@hotmail.com>
2022-08-20 00:12:59 -0300

    refactor: lint: Rotas

--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -1,24 +1,25 @@
-import { Routes, Route } from "react-router-dom"
-import myPath from "./myPaths"
+/* eslint-disable react/react-in-jsx-scope */
+import { Routes, Route } from 'react-router-dom'
+import myPath from './myPaths'
 
-import Intro from "../pages/Intro"
-import QuemSouEu from "../pages/QuemEuSou"
-import OQueEuSei from "../pages/OQueEuSei"
-import OQueEuNaoSei from "../pages/OQueEuNaoSei"
-import OQueEuQuero from "../pages/OQueEuQuero"
-import MeusProjetos from "../pages/MeusProjetos"
+import Intro from '../pages/Intro'
+import QuemSouEu from '../pages/QuemEuSou'
+import OQueEuSei from '../pages/OQueEuSei'
+import OQueEuNaoSei from '../pages/OQueEuNaoSei'
+import OQueEuQuero from '../pages/OQueEuQuero'
+import MeusProjetos from '../pages/MeusProjetos'
 
 import imgs from '../midia/midiaHub'
 
 export default function MyRoutes() {
-  return(
-    <Routes>
-      <Route path='/' element={ <Intro /> } />
-      <Route path={ myPath.quemSou } element={ <QuemSouEu avatar={imgs.eu} /> } />
-      <Route path={ myPath.oQueSei } element={ <OQueEuSei avatar={imgs.teclando} /> } />
-      <Route path={ myPath.oQQuero } element={ <OQueEuQuero /> } />
-      <Route path={ myPath.oQueNaoSei } element={ <OQueEuNaoSei /> } />
-      <Route path={ myPath.projetos } element={ <MeusProjetos /> } />
-    </Routes> 
-  )
+	return(
+		<Routes>
+			<Route path='/' element={ <Intro /> } />
+			<Route path={ myPath.quemSou } element={ <QuemSouEu avatar={imgs.eu} /> } />
+			<Route path={ myPath.oQueSei } element={ <OQueEuSei avatar={imgs.teclando} /> } />
+			<Route path={ myPath.oQQuero } element={ <OQueEuQuero /> } />
+			<Route path={ myPath.oQueNaoSei } element={ <OQueEuNaoSei /> } />
+			<Route path={ myPath.projetos } element={ <MeusProjetos /> } />
+		</Routes> 
+	)
 }
