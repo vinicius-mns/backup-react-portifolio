8914f806ab007f0bae2bdf49457fc7d4753b45a6
vinicius silva <vinicius_dev@hotmail.com>
2022-08-19 23:40:44 -0300

    refactor: lint: Contatcts

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -1,34 +1,32 @@
+import React from 'react'
+import { Props } from '../../types/ReacProps'
 import { useState } from 'react'
 import './style.css'
+ 
+const Contacts: React.FC<Props> = ({ resolve }) => {
+	const [ e, setE ] = useState( false )
 
-// interface Props extends React.Component {
-//   resolve: 'inIntro' | 'inNavBar'
-// }
-
-export default function Contacts({resolve}: any ) {
-  const [ e, setE ] = useState(false)
-
-  return (
-    <div className={resolve}>
-      <a href="https://www.linkedin.com/in/vinicius-dev-junior/" target='blank'>
-        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkedin" />
-      </a>
-      <a href="https://github.com/vinicius-mns" target='blank'>
-        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github" />
-      </a>
-      <img 
-        src="https://cdn-icons-png.flaticon.com/512/561/561127.png"
-        alt="Email"
-        onClick={() => setE( true )}
-      />
-      { e && 
-        <div className='email' onMouseLeave={() => setE( false )}>
-          <p onClick={() => setE( false )}>X</p>
-          <h2>
-            vinicius_dev@hotmail.com
-          </h2>
-        </div>
-      }
-    </div>
-  )
+	return (
+		<div className={resolve}>
+			<a href="https://www.linkedin.com/in/vinicius-dev-junior/" target='blank'>
+				<img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkedin" />
+			</a>
+			<a href="https://github.com/vinicius-mns" target='blank'>
+				<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github" />
+			</a>
+			<img 
+				src="https://cdn-icons-png.flaticon.com/512/561/561127.png"
+				alt="Email"
+				onClick={() => setE( true )}
+			/>
+			{ e && 
+				<div className='email' onMouseLeave={() => setE( false )}>
+					<p onClick={() => setE( false )}>X</p>
+					<h2>vinicius_dev@hotmail.com</h2>
+				</div>
+			}
+		</div>
+	)
 }
+
+export default Contacts
