8c7209f61756dfd943aa55682081eeedf2c4665d
vinicius silva <vinicius_dev@hotmail.com>
2022-08-22 16:25:52 -0300

    refactor: Cria SCSS `intro`

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import './style.css'
+import './style.scss'
 import image from '../../midia/saudacao.gif'
 import Contacts from '../../components/contacts'
 
 const Intro = () => {
 	return(
 		<div className='intro'>
-			<div className='quadrox displayFlex'>
+			<div className='container_img' >
 				<img className='avatarx' src={image} alt='boneco modelo 3d' />
 			</div>
-			<div className='introx'>
-				<h1>Bem   Vindo</h1>
+			<div className='container_content'>
+				<h1>Bem Vindo</h1>
 				<p>Olá, eu me chamo Vinicius e este é meu portifolio.</p>
 				<p>Fique a vontada para usar a barra leteral e me conhecer melhor</p>
 				<Contacts resolve='inIntro' />
