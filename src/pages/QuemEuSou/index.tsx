b8814861beaa5cbd476246628578d729e26b32ab
vinicius silva <vinicius_dev@hotmail.com>
2022-08-22 16:25:16 -0300

    refactor: Cria SCSS `quem sou eu`

--- src/pages/QuemEuSou/index.tsx
+++ src/pages/QuemEuSou/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import './style.css'
+import './style.scss'
 import { img_description, i_am } from './content'
 import Contacts from '../../components/contacts'
 import { oQueEuSei } from '../../types/ReacProps'
 
 const QuemSouEu: React.FC<oQueEuSei> = ({avatar}) => {
 	return(
-		<div className='box'>
-			<div className='quadro'>
+		<div className='container_qes'>
+			<div className='container_img'>
 				<img className='avatar' src={avatar} alt={img_description} />
 			</div>
-			<div className='text'>
+			<div className='container_text'>
 				<h1>V i n í c i u s</h1>
 				{ i_am.map( ( me, index ) => <p key={ index } >{ me }</p> ) }
 			</div>
