4ce4f91e5c6bcdafca36cd8d6b2ffca18862d85f
vinicius silva <vinicius_dev@hotmail.com>
2022-08-23 20:36:43 -0300

    adiciona pagina `oq eu nao sei`

--- src/pages/OQueEuNaoSei/index.tsx
+++ src/pages/OQueEuNaoSei/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './style.scss'
 
 export default function OQueEuNaoSei() {
 	return (
-		<div>
+		<div className='container-oqns'>
 			<h1>O que eu nao sei fazer</h1>
 		</div>
 	)
