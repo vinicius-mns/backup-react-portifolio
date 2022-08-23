4443ad3dca6407d369caf0a6a550c9362523458e
vinicius silva <vinicius_dev@hotmail.com>
2022-08-23 20:00:07 -0300

    feat: Adiciona hard skills a pagina `oq eu sei`

--- src/midia/midiaHub.ts
+++ src/midia/midiaHub.ts
@@ -1,9 +1,11 @@
 import eu from './avatar.gif'
 import teclando from './teclando.gif'
+import soft from './soft.gif'
 
 const imgs = {
 	eu,
 	teclando,
+	soft,
 }
 
 export default imgs
--- src/pages/OQueEuSei/index.tsx
+++ src/pages/OQueEuSei/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './style.scss'
-import { certificados, soft } from './content'
+import { certificados, soft, hard } from './content'
 import { oQueEuSei } from '../../types/ReacProps'
 import Contacts from '../../components/contacts'
 
@@ -10,9 +10,23 @@ const OQueEuSei: React.FC<oQueEuSei> = ({avatar}) => {
 			<div className='container_img'>
 				<img src={avatar} alt="eu" />
 			</div>
-			<div className='sub_container'>
+			<div className='text_container'>
 				<h1>Soft Skills</h1>
 				{ soft.map( ({titulo, content}, index ) => (
+					<div key={index} className='skils'>
+						<h2>{titulo}</h2>
+						<div>
+							{content.map( ( x, i ) => <p key={i} >{x}</p> )}
+						</div>
+					</div>
+				) )}
+			</div>
+			<div className='container_img'>
+				<img src={avatar} alt="eu" />
+			</div>
+			<div className='text_container'>
+				<h1>Hard Skills</h1>
+				{ hard.map( ({titulo, content}, index ) => (
 					<div key={index} className='skils'>
 						<h2>{titulo}</h2>
 						<div>
