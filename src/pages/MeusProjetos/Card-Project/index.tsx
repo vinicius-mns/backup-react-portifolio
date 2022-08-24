bae0915950d869af8c01874174c9413fc4040616
vinicius silva <vinicius_dev@hotmail.com>
2022-08-24 19:33:40 -0300

    feat: Adiciona mock de conteudo para visualizacao do SCC do component `card-project`

--- src/pages/MeusProjetos/Card-Project/index.tsx
+++ src/pages/MeusProjetos/Card-Project/index.tsx
@@ -1,18 +1,67 @@
+/* eslint-disable max-len */
 import React from 'react'
 import './style.scss'
 
 const CardProject = () => {
 	return (
 		<div className='project-card'>
-			<div>
+			<div className='container-img'>
 				<h1>Titulo</h1>
 				<div>container da img</div>
 			</div>
-			<div>
-				<div> aqui vai a descricao </div>
-				<div>
-					<div><span>Site</span> <span>Repositorio</span></div>
-					<div> aqui vai as stacks </div>
+			<div className='container-content'>
+				<div className='description'>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus laudantium magnam architecto earum inventore nulla pariatur quisquam veniam voluptatem, ea corporis repudiandae quod voluptatibus dolor aspernatur voluptate, fugit illum aut. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio veritatis consequatur incidunt consectetur nulla enim ratione officia qui quibusdam ipsum quae cupiditate et recusandae suscipit alias sunt debitis, illo adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sint necessitatibus ipsam asperiores fuga nulla blanditiis saepe. Doloribus id recusandae explicabo labore facilis in sapiente ducimus eaque veritatis voluptates. Earum.</p>
+				</div>
+				<div className='stacks-link'>
+					<div className='link'>
+						<a href='/nada'>Site</a>
+						<a href='/outro'>Repositório</a>
+					</div>
+					<div className='stacks'>
+						<div>
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
+						</div>
+						<div>
+							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
+						</div>
+						<div>
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
+						</div>
+						<div>
+							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
+						</div>
+						<div>
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
+						</div>
+						<div>
+							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
+						</div>
+						<div>
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
+						</div>
+						<div>
+							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
+						</div>
+						<div>
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
+						</div>
+						<div>
+							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
+						</div>
+						<div>
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
+						</div>
+						<div>
+							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
+						</div>
+						<div>
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
+						</div>
+						<div>
+							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
