97d91b52d8318c40efad9ce6f32c69be6b8e93cd
vinicius silva <vinicius_dev@hotmail.com>
2022-08-25 15:54:57 -0300

    feat: adiciona props ao `card-project`

--- src/pages/MeusProjetos/Card-Project/index.tsx
+++ src/pages/MeusProjetos/Card-Project/index.tsx
@@ -1,66 +1,26 @@
 /* eslint-disable max-len */
 import React from 'react'
 import './style.scss'
+import { cardProject } from '../../../types/ReacProps'
 
-const CardProject = () => {
+const CardProject: React.FC<cardProject> = ({image, title, text, site, repo, stacks}) => {
 	return (
 		<div className='project-card'>
 			<div className='container-img'>
-				<h1>Titulo</h1>
-				<div>container da img</div>
+				<div><img src={image.image} alt={image.desc}/></div>
 			</div>
 			<div className='container-content'>
 				<div className='description'>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus laudantium magnam architecto earum inventore nulla pariatur quisquam veniam voluptatem, ea corporis repudiandae quod voluptatibus dolor aspernatur voluptate, fugit illum aut. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio veritatis consequatur incidunt consectetur nulla enim ratione officia qui quibusdam ipsum quae cupiditate et recusandae suscipit alias sunt debitis, illo adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sint necessitatibus ipsam asperiores fuga nulla blanditiis saepe. Doloribus id recusandae explicabo labore facilis in sapiente ducimus eaque veritatis voluptates. Earum.</p>
+					<h1>{title}</h1>
+					{text.map( ( paragrafo, i ) => <p key={i}>{paragrafo}</p> )}
 				</div>
 				<div className='stacks-link'>
 					<div className='link'>
-						<a href='/nada'>Site</a>
-						<a href='/outro'>Repositório</a>
+						{site &&  <a href={site}>Site</a>}
+						{repo && <a href={repo}>Respositório</a>}
 					</div>
 					<div className='stacks'>
-						<div>
-							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
-						</div>
-						<div>
-							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
-						</div>
-						<div>
-							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
-						</div>
-						<div>
-							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
-						</div>
-						<div>
-							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
-						</div>
-						<div>
-							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
-						</div>
-						<div>
-							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
-						</div>
-						<div>
-							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
-						</div>
-						<div>
-							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
-						</div>
-						<div>
-							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
-						</div>
-						<div>
-							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
-						</div>
-						<div>
-							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
-						</div>
-						<div>
-							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt=""/>
-						</div>
-						<div>
-							<img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png" alt=""/>
-						</div>
+						{stacks.map( ( s, i ) => <div key={i}><img src={s.image} alt={s.desc}/></div> )}
 					</div>
 				</div>
 			</div>
