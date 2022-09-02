282fcef67120a439ebe857679184a1059755b39b
vinicius silva <vinicius_dev@hotmail.com>
2022-09-02 19:06:30 -0300

    feat: Login page

---
+++ src/pages/Users/logins.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+	return (
+		<main>
+			<input type="text" name="" id="" placeholder='Nome'/>
+			<input type="password" name="" id="" placeholder='Senha'/>
+			<button>Logar</button>
+		</main>
+	)
+}
+
+export default Login
