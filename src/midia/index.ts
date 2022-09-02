f3d455f91739f560fb8f51c6b337f275f2eef163
vinicius silva <vinicius_dev@hotmail.com>
2022-09-02 18:56:52 -0300

    refactor midia hub

--- src/midia/index.ts
+++ src/midia/index.ts
@@ -1,10 +1,13 @@
 import eu from './avatar.gif'
 import teclando from './teclando.gif'
+import saudacao from './saudacao.gif'
 import soft from './soft.gif'
+
 const imgs = {
 	eu,
 	teclando,
 	soft,
+	saudacao,
 }
 
 export default imgs
