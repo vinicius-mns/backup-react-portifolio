61c7ebb6f170e90bbb6c2cd516af3092ec1d96d7
vinicius silva <vinicius_dev@hotmail.com>
2022-09-30 10:14:19 -0300

    refacor: Removendo coisas descessarias na pagina de introducao

--- src/pages/Navigations/Intro/index.tsx
+++ src/pages/Navigations/Intro/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './style.scss'
 import image from '../../../midia'
 import Contacts from '../../../components/contacts'
-import Login from '../../Users/Login'
 
 const Intro = () => {
 	return(
