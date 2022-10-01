09e782685731db57bb5f177344ad8c34682bd3e6
vinicius silva <vinicius_dev@hotmail.com>
2022-10-01 05:16:33 -0300

    feat: Cria slice/reducer para introCard - toogleComponents

--- src/redux/slices/toggleComponents.ts
+++ src/redux/slices/toggleComponents.ts
@@ -4,14 +4,18 @@ const toggleComponents = createSlice({
 	name: 'toggleComponents',
 	initialState: {
 		sideBar: '',
+		introCard: '',
 	},
 	reducers: {
 		showSideBar: (state) => {
 			state.sideBar = 'showSideBar'
+		},
+		slideIntroCard: (state) => {
+			state.introCard = 'slideIntroCard'
 		}
 	}
 })
 
-export const { showSideBar } = toggleComponents.actions
+export const { showSideBar, slideIntroCard } = toggleComponents.actions
 
 export default toggleComponents
