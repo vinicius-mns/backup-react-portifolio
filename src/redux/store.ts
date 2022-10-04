f3e24239a3fd35357153a75062549566309dbce8
vinicius silva <vinicius_dev@hotmail.com>
2022-10-04 09:39:29 -0300

    feat: Adiciona (estado global) `quemSouEu` ao Store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import quemEuSou, { IamInterface } from './slices/quemEuSou'
 import appSlice from './slices/toggleComponents'
 
 const store = configureStore({
 	reducer: {
 		toggleComponents: appSlice.reducer,
+		quemEuSou: quemEuSou.reducer,
 	}
 })
 
@@ -11,7 +13,8 @@ export type Store = {
 	toggleComponents: { 
 		sideBar: 'showSideBar' | '',
 		introCard: 'slideIntroCard' | '',
-	}
+	},
+	quemEuSou: IamInterface,
 }
 
 export default store
