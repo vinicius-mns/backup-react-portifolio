ce7343766912dcd8930add9a048e3a2ae9a3be12
vinicius silva <vinicius_dev@hotmail.com>
2022-10-04 10:09:07 -0300

    feat: `QuemSouEu` Atualiza com os dados do backend quando renderiza

--- src/pages/Navigations/QuemEuSou/index.tsx
+++ src/pages/Navigations/QuemEuSou/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './style.scss'
-import { img_description, i_am } from './content'
-import Contacts from '../../../components/contacts'
 import { oQueEuSei } from '../../../types/ReacProps'
 import ReactMarkdown from 'react-markdown'
 import { useSelector } from 'react-redux'
 import { Store } from '../../../redux/store'
+import { useDispatch } from 'react-redux'
+import MyAxios from '../../../classes/MyAxios'
+import { setQuemEuSou } from '../../../redux/slices/quemEuSou' 
 import Contacts from '../../../components/contacts'
 
 const QuemSouEu: React.FC<oQueEuSei> = () => {
 	const iam = useSelector((state: Store) => state.quemEuSou)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		const iamAxios = new MyAxios('https://v-portifolio-backend.herokuapp.com/iam')
+		iamAxios.getOne().then(e => dispatch(setQuemEuSou(e)))
+	})
 
 	return(
 		<div className='container_qes'>
