dcc22c3f3f0729c8442117e1ba88edbce163bf51
vinicius silva <vinicius_dev@hotmail.com>
2022-10-04 15:00:28 -0300

    feat: Implementa fetch a Api nas rotas

--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -10,10 +10,27 @@ import OQueEuQuero from '../pages/Navigations/OQueEuQuero'
 import MeusProjetos from '../pages/Navigations/MeusProjetos'
 import Portifolio from '../pages/Navigations/Portifolio'
 import FeedBacks from '../pages/Navigations/FeedBacks'
-
 import imgs from '../midia'
 
+import MyAxios from '../classes/MyAxios'
+
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { setQuemEuSou } from '../redux/slices/quemEuSou'
+import { useSelector } from 'react-redux'
+import { Store } from '../redux/store'
+
 export default function MyRoutes() {
+	const dispatch = useDispatch()
+	const state = useSelector((state: Store) => state)
+
+	useEffect(() => {
+		const iamAxios = new MyAxios('https://v-portifolio-backend.herokuapp.com/iam')
+		!state.quemEuSou._id && iamAxios.getOne().then(quemSou => dispatch(setQuemEuSou(quemSou)))
+
+		console.log(state)
+	}, [])
+
 	return(
 		<Routes>
 			<Route path='/' element={ <Intro /> } />
