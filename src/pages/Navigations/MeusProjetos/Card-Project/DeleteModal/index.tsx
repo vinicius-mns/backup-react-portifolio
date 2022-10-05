9279f7176f2d5e97ae11881a8e43e690396d8096
vinicius silva <vinicius_dev@hotmail.com>
2022-10-05 05:06:21 -0300

    feat: Cria Modal para deletar projeto

---
+++ src/pages/Navigations/MeusProjetos/Card-Project/DeleteModal/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.scss'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { useDispatch } from 'react-redux'
+import { setModalDeleteProject } from '../../../../../redux/slices/toggleComponents'
+
+const ModalDeleteProject = () => {
+	const dispatch = useDispatch()
+	return (
+		<div className='modalDeleteProject'>
+			<button className='close' onClick={() => dispatch(setModalDeleteProject())}>X</button>
+			<div><h1>Realmente deseja deletar o Projeto ?</h1></div>
+			<div>
+				<ReactMarkdown>conteudo</ReactMarkdown>
+			</div>
+			<button><h2>Confirmar</h2></button>
+		</div>
+	)
+}
+
+export default ModalDeleteProject
