a3f0fb73bbb925a0cd49faad0211d689dba5c603
vinicius silva <vinicius_dev@hotmail.com>
2022-10-05 03:12:52 -0300

    feat: Cria slice (estado global) `projetos`

---
+++ src/redux/slices/projetos.ts
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface ProjectInterface {
+	image: { src: string, alt: string },
+  title: string,
+  description: string,
+  repository: string,
+  site: string,
+  stacks: { src: string, alt: string }[],
+}
+
+const projetos = createSlice({
+	name: 'projetos',
+	initialState: [
+		{
+			image: {src: '', alt: ''},
+			title: '',
+			description: '',
+			repository: '',
+			site: '',
+			stacks: [ {src: '', alt: ''} ],
+		}
+	],
+	reducers: {
+		setProjetos: (_state, action: {payload: ProjectInterface[]}) => {
+			return action.payload
+		}
+	}
+})
+
+export const { setProjetos } = projetos.actions
+
+export default projetos
