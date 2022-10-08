3eadf623e383de11e40b33101d20cc49d378cb0d
vinicius silva <vinicius_dev@hotmail.com>
2022-10-08 10:52:01 -0300

    refactor: Renderiza o componente `login` em qualquer pagina

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import './styles/App.scss'
 import { useDispatch } from 'react-redux'
 import { toggleLogin } from './redux/slices/toggleComponents'
-
+import Login from './pages/Users/Login'
 
 import NavBackground from './components/navBar'
 
 import MyRoutes from './routes/MyRoutes'
+import { useSelector } from 'react-redux'
+import { Store } from './redux/store'
 
 function App() {
 	const dispatch = useDispatch()
+	const toggleState = useSelector((state: Store) => state.toggleComponents)
+
 	return (
 		<div>
+			{ toggleState.login && <Login /> }
 			<button className='login' onClick={() => dispatch(toggleLogin())}>?</button>
 			<div>
 				<NavBackground />
